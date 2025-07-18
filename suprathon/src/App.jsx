f9959a8854de875dde397fa4c4825feae6d2eeb4
Mithil1302 <mithilpatel1302@gmail.com>
2025-07-19 03:04:54 +0530

    User Signup

--- suprathon/src/App.jsx
+++ suprathon/src/App.jsx
@@ -1,15 +1,10 @@
-import { useState } from 'react'
 import './App.css'
-import { Button } from "@/components/ui/button"
+import UserSignup from './User-Signup'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="h-screen flex items-center justify-center">
-      <h1 className="text-amber-400 text-4xl">Lets get this shit done !</h1>
-      <Button>Welcome</Button>
-    </div>
+      <UserSignup/>
   )
 }
 
